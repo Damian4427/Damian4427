05c411c8ae2f60e677be53a0edff76efdd6c74f2
Damian Gomez <gomezdamian1234@gmail.com>
2024-04-11 19:37:52 -0300

    pequeños cambios en css

--- js/productos.js
+++ js/productos.js
@@ -9,8 +9,8 @@ function procesarProductos(producto) {
         <div class="card-name">${producto.nombre}</div>
         <div class="card-price">$ ${producto.precio}</div>
         <div class="card-button">
-            <button class="button button-outline button-add" id="${producto.id}" title="Clic para agregar al carrito">Agregar al carrito</button>
-        </div>
+        <button class="button button-outline button-add" id="${producto.id}" title="Clic para agregar al carrito">Agregar al carrito</button>
+            </div>
     `;
 
     return cardElement;
